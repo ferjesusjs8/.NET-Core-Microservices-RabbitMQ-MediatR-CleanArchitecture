742bae72c40fd402e42cd627a7c6925b52826b14
fernando.js8 <fernando.js8@outlook.com>
2022-01-18 16:46:49 -0300

    WIP

---
+++ Course/FirstLesson/FirstLesson/Consumer/Receiver.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        static int i = 0;
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "BasicTest", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += Consumer_Received;
+
+                    channel.BasicConsume(queue: "BasicTest", autoAck: true, consumer: consumer);
+
+                    Console.WriteLine("Press [enter] to exit the consumer.");
+                }
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.Span;
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine($"Received the message {i} x {message}");
+            i++;
+        }
+    }
+}
---
+++ Course/FirstLesson/FirstLesson/Producer/Sender.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                    string message = "Getting started with .Net Core RabbitMQ";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    for (int i = 0; i < 100; i++)
+                    {
+                        channel.BasicPublish("", "BasicTest", null, body);
+
+                        Console.WriteLine($"Sent message {i} x {message}");
+                    }
+                }
+            }
+
+            Console.WriteLine("Press [enter] to exit the Sender App...");
+        }
+    }
+}
