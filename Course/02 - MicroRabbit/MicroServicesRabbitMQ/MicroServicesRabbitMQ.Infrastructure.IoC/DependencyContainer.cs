17c164a41a7576a47fd2f25d325f55c7e6980186
fernando.js8 <fernando.js8@outlook.com>
2022-01-19 16:39:57 -0300

    initial commit, Dependency container class added, RegisterServices Method added

---
+++ Course/02 - MicroRabbit/MicroServicesRabbitMQ/MicroServicesRabbitMQ.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicroServicesRabbitMQ.Domain.Core.Bus;
+using MicroServicesRabbitMQ.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroServicesRabbitMQ.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
