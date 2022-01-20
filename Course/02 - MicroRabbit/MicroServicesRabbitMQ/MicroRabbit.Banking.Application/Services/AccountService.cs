060a17d243a947db4dfdab79189f49a084b3c511
fernando.js8 <fernando.js8@outlook.com>
2022-01-20 13:35:59 -0300

    Account services, repositories and interfaces implemented

---
+++ Course/02 - MicroRabbit/MicroServicesRabbitMQ/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Banking.Application.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _repository;
+
+        public AccountService(IAccountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _repository.GetAccounts();
+        }
+    }
+}
