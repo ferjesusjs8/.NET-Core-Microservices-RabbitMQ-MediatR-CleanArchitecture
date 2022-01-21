88d8154da5993498a6bc6014780b261e818a6f6c
fernando.js8 <fernando.js8@outlook.com>
2022-01-20 23:18:59 -0300

    implemented Publish and SendCommand for Transfer Event

--- Course/02 - MicroRabbit/MicroServicesRabbitMQ/MicroRabbit.Banking.Application/Services/AccountService.cs
+++ Course/02 - MicroRabbit/MicroServicesRabbitMQ/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -1,6 +1,9 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
+using MicroServicesRabbitMQ.Domain.Core.Bus;
 using System.Collections.Generic;
 
 namespace MicroRabbit.Banking.Application.Services
@@ -8,15 +11,27 @@ namespace MicroRabbit.Banking.Application.Services
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _repository;
+        private readonly IEventBus _eventBus;
 
-        public AccountService(IAccountRepository repository)
+        public AccountService(IAccountRepository repository, IEventBus eventBus)
         {
             _repository = repository;
+            _eventBus = eventBus;
         }
 
         public IEnumerable<Account> GetAccounts()
         {
             return _repository.GetAccounts();
+        }
+
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            var createTransferCommand = new CreateTransferCommand(
+                accountTransfer.FromAccount,
+                accountTransfer.ToAccount,
+                accountTransfer.TransferAmount);
+
+            _eventBus.SendCommand(createTransferCommand);
         }
     }
 }
